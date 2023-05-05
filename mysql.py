2b3e14d3dfdc72d0b3afbd313b97dbaadce00574
Galia <113383623+galia-jalili@users.noreply.github.com>
2023-05-05 19:40:22 +0330

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -9,8 +9,6 @@ mydb = mysql.connector.connect(
 
 # create a table named "programs"
 mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE programs (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), asset_identifier VARCHAR(255, type VARCHAR(255), platform VARCHAR(255))")
-
-
+mycursor.execute("CREATE TABLE programs (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), Asset_Identifier VARCHAR(1000), type VARCHAR(255), platform VARCHAR(255))")
 mydb.commit()
 mydb.close()
